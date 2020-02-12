18f1180e0f6d7dce71e99936bf00357b31ce2d52
mugdha <mugdha.wadhokar@gmail.com>
2020-02-12 18:32:03 +0530

    Update remove-duplicate-sorted-array.php
    Reduce the code complexity.

--- remove-duplicate-sorted-array.php
+++ remove-duplicate-sorted-array.php
@@ -1,25 +1,24 @@
 <?php
 
 $nums = [0,0,1,1,2,2,3,3,4];
-$len = removeDuplicates($nums);
+$intLen = removeDuplicates($nums);
 
-echo "length ::".$len;
-
+echo "length ::".$intLen;
 print_r($nums);
-function removeDuplicates (&$nums) {
-    for ($i = 0, $j = 0; $i < count($nums); $i++) {
-        if ($i == 0) {
-            $number =  $nums[$i];
+
+
+function removeDuplicates(&$nums) {
+    if (count($nums) === 0) {
+        return 0;
+    }
+
+    for ($i = 1, $j = 0; $i < count($nums); $i++) {
+        if ($nums[$j] != $nums[$i]) {
             $j++;
-            continue;
-        }
-        if ($number != $nums[$i]) {
-            $number =  $nums[$i];
             $nums[$j] = $nums[$i];
-            $j++;
         }
     }
 
-    return $j;
+    return $j+1;
 }
 ?>
