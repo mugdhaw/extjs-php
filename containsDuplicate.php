c86cc2b48d990cdda772eec5fed7cc3f1daf5b6f
mugdha <mugdha.wadhokar@gmail.com>
2020-02-14 18:17:39 +0530

    Create containsDuplicate.php
    Given an array of integers, find if the array contains any duplicates.
    
    Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
    
    Example 1:
    Input: [1,2,3,1]
    Output: true
    
    Example 2:
    Input: [1,2,3,4]
    Output: false

---
+++ containsDuplicate.php
@@ -0,0 +1,16 @@
+<?php
+
+$nums = [7,1,5,3,6,4];
+$boolIsDuplicatePresent = containsDuplicate($nums);
+
+/**
+ * @param Integer[] $nums
+ * @return Boolean
+ */
+function containsDuplicate($nums) {
+
+
+
+}
+
+?>
