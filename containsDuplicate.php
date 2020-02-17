d260fbf8f55e6f1b322ef657886160e50f78d3b1
mugdha <mugdha.wadhokar@gmail.com>
2020-02-17 12:45:42 +0530

    Update containsDuplicate.php
    Complexity Analysis
    
    Time complexity : O(nlogn). Sorting is O(nlogn) and the sweeping is O(n). Since comparison sorting algorithm like heapsort is known to provide O(nlogn) worst-case performance, sorting is often a good preprocessing step. The entire algorithm is dominated by the sorting step, which is O(nlogn).
    
    Space complexity : O(1). Space depends on the sorting implementation which, usually, costs O(1) auxiliary space if heapsort is used.

--- containsDuplicate.php
+++ containsDuplicate.php
@@ -1,15 +1,19 @@
 <?php
 
-$nums = [7,1,5,3,6,4];
+$nums = [7,1,5,3,6,7];
 $boolIsDuplicatePresent = containsDuplicate($nums);
 
+echo "Is Duplicate ::".$boolIsDuplicatePresent;
 /**
  * @param Integer[] $nums
  * @return Boolean
  */
 function containsDuplicate($nums) {
 
-
+    sort($nums); // sorting array first will take less time than traversing through each and every number in the array using two forloop
+    for($i = 0; $i < count($nums) - 1; $i++) {
+        if ($nums[$i] == $nums[$i+1]) return true;
+    }
 
 }
 
